6537cff0bd294398ecaccd038f4b961f9e8e80cb
codeclub <b211407@rgukt.ac.in>
2026-06-29 10:25:32 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,11 +1,7 @@
-import java.util.Scanner;
-
 public class Pattern {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of rows: ");
-        int n = sc.nextInt();
+        int n = Integer.parseInt(args[0]);
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
